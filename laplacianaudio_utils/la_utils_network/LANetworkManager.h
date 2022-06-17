0626b3e4b668a70fa0fa36a20de346ba3e1d380a
Mike Gazzaruso <mike@gazzaruso.com>
2022-06-17 20:10:23 +0200

    Implement Main Network Business Logic

--- laplacianaudio_utils/la_utils_network/LANetworkManager.h
+++ laplacianaudio_utils/la_utils_network/LANetworkManager.h
@@ -1,18 +1,34 @@
 #pragma once
-#ifndef LANetworkManager_h
-#define LANetworkManager_h
 
 #include <JuceHeader.h>
+
+typedef std::function<void (juce::String)> RequestCallback;
 
 namespace la_utils_network
 {
     class LANetworkManager
+    :   private juce::Thread
     {
     public:
         LANetworkManager();
         ~LANetworkManager();
+        
+        void run() override;
+        
+        juce::String getResultText(const juce::URL& url);
+        
+        void setUrlString(const juce::String inUrlString) { mUrlString = inUrlString; }
+        
+        void makeFetchRequest(RequestCallback cb);
+        
+    private:
+        juce::String mUrlString;
+        RequestCallback mRequestCallback;
+        
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LANetworkManager)
+
     };
+
 }
 
-#endif /** LANetworkManager_h */
         
